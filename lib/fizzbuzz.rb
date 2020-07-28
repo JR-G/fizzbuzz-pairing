03d8ed17aebec52c6482dddcf2384caec46cc9a7
JRG <47917431+JR-G@users.noreply.github.com>
2020-07-28 16:07:01 +0100

    updated fizzbuzz to include 'buzz' & 'fizzbuzz'

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,5 +1,11 @@
 def fizzbuzz(number)
-  if number % 3 == 0
-    return "fizz"
+  fizz = (number % 3 == 0)
+  buzz = (number % 5 == 0)
+  case
+    when fizz && buzz then "fizzbuzz"
+    when fizz then "fizz"
+    when buzz then "buzz"
+  else number
   end
+
 end
